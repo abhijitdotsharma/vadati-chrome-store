cbe55c00e1672676202aeeac08e5b87190f212fc
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-11-17 17:07:40 +0530

    add: <Enquire /> with enquire.css

--- src/components/enquire/Enquire.jsx
+++ src/components/enquire/Enquire.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react";
+import "./enquire.css";
 
-export default function Enquire(){
+export default function Enquire({ user, setUser }) {
 
-    return(
-        <div
-        style={{
-            border:'1px solid',
-            width: '20rem',
-            margin: 'auto',
-        }}
-        >
-            <h3>What is your name ? </h3>
+    const [userName, setUserName] = useState('');
+    
+
+
+    function inputHandler(event) {
+        setUserName(event.target.value)
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        
+        if(userName.trim().length === 0){
+            alert('enter valid name')
+        }else if(userName.trim().length > 20){
+            alert("Name too long, enter a short name")
+        }else{
+            //change the local storage value 
+            setUser("userName", userName)
+        }
+        
+
+    }
+    console.log('after setIsLoggedIn - ', user?.isLoggedIn)
+
+    return (
+        <div className="enquire">
+
+            <form className="enquire__form">
+                <p className="enquire__heading">What is your name ? </p>
+
+                <input
+                    className="enquire__input"
+                    onSubmit={submitHandler}
+                    type="text"
+                    value={userName}
+                    onChange={inputHandler}
+                />
+
+                <button 
+                className="enquire__submit btn btn-large btn-primary"
+                onClick={submitHandler}
+                >Primary</button>
+            </form>
         </div>
     )
 }
